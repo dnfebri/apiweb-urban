e68a3ccc6a3e6e0011be91f73d2b9fb520413885
dnfebri <dwinofebri@gmail.com>
2022-10-25 20:11:13 +0700

    cors

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ app.use(cors(
 //   next();
 // });
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(FileUpload())
